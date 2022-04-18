cb95aff5ad6ca8b46b80f16ab24596f5742e9652
gaohui01 <gaohui01@5i5j.com>
2022-04-18 17:22:30 +0800

    组件化雏形,修改配置文件

--- lib_core/src/main/java/com/gh/lib_core/baseui/BaseApplication.kt
+++ lib_core/src/main/java/com/gh/lib_core/baseui/BaseApplication.kt
@@ -16,7 +16,18 @@ open class BaseApplication : Application() {
         super.onCreate()
 
         MMKV.initialize(this)
-        ARouter.init(this)
+
+        initArouter()
+
+    }
+
+    private fun initArouter() {
+
+//        if (isDebug()) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
+        ARouter.openLog();     // 打印日志
+        ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+//        }
+        ARouter.init(this); // 尽可能早，推荐在Application中初始化
 
     }
 
